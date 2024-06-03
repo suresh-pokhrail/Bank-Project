497af6d896105987871a3a258ada5d972bd1609a
suresh-pokhrail <97016784+suresh-pokhrail@users.noreply.github.com>
2024-06-03 21:09:30 +0530

    Add files via upload

---
+++ accounts/src/main/java/com/bank/accounts/entity/Accounts.java
@@ -0,0 +1,30 @@
+package com.bank.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity 
+@Getter
+@Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Accounts extends BaseEntity {
+
+	@Id
+	@Column(name = "account_number")
+	private long accountNumber;
+	
+	@Column(name = "customer_id")
+	private long custId;
+	
+	@Column(name = "account_type")
+	private String accountType;
+	
+	@Column(name = "branch_address")
+	private String branchAddress;
+	
+}
